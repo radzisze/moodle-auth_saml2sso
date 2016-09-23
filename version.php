1196ab528f69ae728a7ebc129ad5ce4cac8867f9
Daniel Miranda <daniellopes@gmail.com>
2016-09-23 16:34:45 -0300

    Added plugin maturity and release in version.php

--- version.php
+++ version.php
@@ -27,3 +27,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version = 2016062800;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires = 2015111000;        // Requires this Moodle version
 $plugin->component = 'auth_saml2_auth';       // Full name of the plugin (used for diagnostics)
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = 'v3.0-r1';
