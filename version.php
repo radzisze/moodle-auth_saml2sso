48255f4568242bd1d4a2f451574c7edf362b9300
Daniel Miranda <daniellopes@gmail.com>
2018-01-15 16:38:41 -0200

    Update release version

--- version.php
+++ version.php
@@ -23,8 +23,8 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2018011401;              // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version = 2018011500;              // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires = 2017051501;             // Requires this Moodle version
 $plugin->component = 'auth_saml2sso';       // Full name of the plugin (used for diagnostics)
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'v3.4-r01';
+$plugin->release = 'v3.4-r02';
